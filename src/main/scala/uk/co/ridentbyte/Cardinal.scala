16396e19505b0d83691c5e927c6eb556300339e6
Benjamin Dale <ben@ben-dale.co.uk>
2018-02-19 21:28:40 +0000

    javaFX 🙌🏼

--- src/main/scala/uk/co/ridentbyte/Cardinal.scala
+++ src/main/scala/uk/co/ridentbyte/Cardinal.scala
@@ -1,5 +1,19 @@
 package uk.co.ridentbyte
 
-object Cardinal extends App {
-  println("Hello World")
+import javafx.application.Application
+import javafx.scene.Scene
+import javafx.scene.layout.StackPane
+import javafx.stage.Stage
+
+object Cardinal {
+  Application.launch()
+}
+
+class Cardinal extends Application {
+  override def start(primaryStage: Stage): Unit = {
+    primaryStage.setTitle("Cardinal")
+    val root = new StackPane()
+    primaryStage.setScene(new Scene(root, 300, 250))
+    primaryStage.show()
+  }
 }
