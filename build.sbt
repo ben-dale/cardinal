741a0b37743ac3db238991319230f20cb6ddb992
Benjamin Dale <ben@ben-dale.co.uk>
2018-02-24 23:28:12 +0000

    request serialisation and deserialisation

--- build.sbt
+++ build.sbt
@@ -3,4 +3,5 @@ organization := "uk.co.ridentbyte"
 name := "cardinal"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.4" % "test"
-libraryDependencies +=  "org.scalaj" %% "scalaj-http" % "2.3.0"
+libraryDependencies += "org.scalaj" %% "scalaj-http" % "2.3.0"
+libraryDependencies += "org.json4s" %% "json4s-jackson" % "3.6.0-M2"
--- src/main/scala/uk/co/ridentbyte/model/Request.scala
+++ src/main/scala/uk/co/ridentbyte/model/Request.scala
@@ -1,3 +1,15 @@
 package uk.co.ridentbyte.model
 
-case class Request(uri: String, verb: String, headers: List[String], body: Option[String])
+import org.json4s.DefaultFormats
+import org.json4s.jackson.JsonMethods.parse
+import org.json4s.jackson.Serialization.write
+
+case class Request(uri: String, verb: String, headers: List[String], body: Option[String]) {
+  private implicit val formats: DefaultFormats = DefaultFormats
+  def toJson: String = write(this)
+}
+
+object Request {
+  private implicit val formats: DefaultFormats = DefaultFormats
+  def apply(json: String): Request = parse(json).extract[Request]
+}
