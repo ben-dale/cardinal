64daec632447462f15e37c30875d154c35ea1558
Benjamin Dale <ben@ben-dale.co.uk>
2018-03-10 13:12:51 +0000

    initial tree view work for files

--- src/main/scala/uk/co/ridentbyte/view/file/FilePane.scala
+++ src/main/scala/uk/co/ridentbyte/view/file/FilePane.scala
@@ -3,7 +3,7 @@ package uk.co.ridentbyte.view.file
 import javafx.beans.value.{ChangeListener, ObservableValue}
 import javafx.geometry.{HPos, Insets}
 import javafx.scene.control.cell.TextFieldListCell
-import javafx.scene.control.{Button, ListView}
+import javafx.scene.control.{Button, ListView, TreeItem, TreeView}
 import javafx.scene.layout._
 
 class FilePane(loadFileCallback: (String) => Unit,
@@ -15,57 +15,51 @@ class FilePane(loadFileCallback: (String) => Unit,
   setVgap(5)
   setPadding(new Insets(10, 10, 10, 10))
 
-  private val listFiles = new ListView[String]
-  listFiles.setEditable(true)
-  listFiles.setCellFactory(TextFieldListCell.forListView())
-  listFiles.getSelectionModel.selectedItemProperty().addListener(new ChangeListener[String] {
-    override def changed(observable: ObservableValue[_ <: String], oldValue: String, newValue: String): Unit = {
+  private val treeFiles = new TreeView[String]()
+  GridPane.setHgrow(treeFiles, Priority.ALWAYS)
+  GridPane.setVgrow(treeFiles, Priority.ALWAYS)
+//  GridPane.setColumnSpan(treeFiles, 2)
+  treeFiles.getSelectionModel.selectedItemProperty().addListener(new ChangeListener[TreeItem[String]] {
+    override def changed(observable: ObservableValue[_ <: TreeItem[String]], oldValue: TreeItem[String], newValue: TreeItem[String]): Unit = {
       if (newValue != null) {
-        loadFileCallback(listFiles.getSelectionModel.getSelectedItem)
+        loadFileCallback(newValue.getValue)
       }
     }
   })
-  listFiles.setOnEditCommit((result) => {
-    val selectedName = listFiles.getSelectionModel.getSelectedItem
-    if (result.getNewValue != null && result.getNewValue.trim.length > 0) {
-      renameFileCallback(selectedName, result.getNewValue)
-    }
-  })
-  GridPane.setHgrow(listFiles, Priority.ALWAYS)
-  GridPane.setVgrow(listFiles, Priority.ALWAYS)
-  GridPane.setColumnSpan(listFiles, 2)
-  add(listFiles, 0, 0)
-
-  private val buttonDelete = new Button("Delete")
-  buttonDelete.setStyle("-fx-padding: 2 6 2 6;")
-  buttonDelete.setOnAction((_) => {
-    val selectedIndex = listFiles.getSelectionModel.getSelectedIndex
-    if (selectedIndex >= 0) {
-      deleteFileCallback(listFiles.getSelectionModel.getSelectedItem)
-    }
-  })
-  GridPane.setHgrow(buttonDelete, Priority.ALWAYS)
-  GridPane.setVgrow(buttonDelete, Priority.NEVER)
-  GridPane.setHalignment(buttonDelete, HPos.RIGHT)
-  add(buttonDelete, 0, 1)
-
-  private val buttonDuplicate = new Button("Duplicate")
-  buttonDuplicate.setStyle("-fx-padding: 2 6 2 6;")
-  buttonDuplicate.setOnAction((_) => {
-    val selectedIndex = listFiles.getSelectionModel.getSelectedIndex
-    if (selectedIndex >= 0) {
-      duplicateFileCallback(listFiles.getSelectionModel.getSelectedItem)
-    }
-  })
-  GridPane.setHgrow(buttonDuplicate, Priority.NEVER)
-  GridPane.setVgrow(buttonDuplicate, Priority.NEVER)
-  add(buttonDuplicate, 1, 1)
+  add(treeFiles, 0, 0)
+//
+//  private val buttonDelete = new Button("Delete")
+//  buttonDelete.setStyle("-fx-padding: 2 6 2 6;")
+//  buttonDelete.setOnAction((_) => {
+//    val selectedIndex = listFiles.getSelectionModel.getSelectedIndex
+//    if (selectedIndex >= 0) {
+//      deleteFileCallback(listFiles.getSelectionModel.getSelectedItem)
+//    }
+//  })
+//  GridPane.setHgrow(buttonDelete, Priority.ALWAYS)
+//  GridPane.setVgrow(buttonDelete, Priority.NEVER)
+//  GridPane.setHalignment(buttonDelete, HPos.RIGHT)
+//  add(buttonDelete, 0, 1)
+//
+//  private val buttonDuplicate = new Button("Duplicate")
+//  buttonDuplicate.setStyle("-fx-padding: 2 6 2 6;")
+//  buttonDuplicate.setOnAction((_) => {
+//    val selectedIndex = listFiles.getSelectionModel.getSelectedIndex
+//    if (selectedIndex >= 0) {
+//      duplicateFileCallback(listFiles.getSelectionModel.getSelectedItem)
+//    }
+//  })
+//  GridPane.setHgrow(buttonDuplicate, Priority.NEVER)
+//  GridPane.setVgrow(buttonDuplicate, Priority.NEVER)
+//  add(buttonDuplicate, 1, 1)
 
   def setListContentTo(files: List[String]): Unit = {
-    listFiles.getItems.clear()
+    val rootItem = new TreeItem[String]("Cardinal")
+    rootItem.setExpanded(true)
     files.sorted.foreach { file =>
-      listFiles.getItems.add(file.replaceFirst(".json", ""))
+      rootItem.getChildren.add(new TreeItem[String](file.replaceFirst(".json", "")))
     }
+    treeFiles.setRoot(rootItem)
   }
 
 }
