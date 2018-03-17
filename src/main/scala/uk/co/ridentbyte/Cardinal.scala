6d2aceb3e81701c35c0654d8dead0e0d1a6c8bed
Benjamin Dale <ben@ben-dale.co.uk>
2018-03-17 18:50:58 +0000

    initial refactoring of view and functions

--- src/main/scala/uk/co/ridentbyte/Cardinal.scala
+++ src/main/scala/uk/co/ridentbyte/Cardinal.scala
@@ -1,23 +1,12 @@
 package uk.co.ridentbyte
 
 import java.io.File
-import java.net.{ConnectException, URISyntaxException, UnknownHostException}
-
-import javafx.application.{Application, Platform}
+import javafx.application.Application
 import javafx.scene.Scene
-import javafx.scene.control.Alert.AlertType
-import javafx.scene.control._
-import javafx.scene.layout.{GridPane, Priority}
 import javafx.stage.Stage
-import javax.net.ssl.SSLHandshakeException
-import uk.co.ridentbyte.model.{BulkRequest, HttpResponseWrapper, Request}
+import uk.co.ridentbyte.model.{HttpResponseWrapper, Request}
 import uk.co.ridentbyte.util.{HttpUtil, IOUtil}
-import uk.co.ridentbyte.view.file.FilePane
-import uk.co.ridentbyte.view.util.{ColumnConstraintsBuilder, RowConstraintsBuilder}
-import uk.co.ridentbyte.view.request.{BulkRequestInputDialog, BulkRequestProcessingDialog, RequestControlPane, RequestInputPane}
-import uk.co.ridentbyte.view.response.{BulkRequestOutputDialog, ResponsePane}
-
-import scala.annotation.tailrec
+import uk.co.ridentbyte.view.CardinalView
 
 object Cardinal {
   def main(args: Array[String]): Unit = {
@@ -28,64 +17,19 @@ object Cardinal {
 class Cardinal extends Application {
 
   private var currentFile: Option[File] = None
-
   private val fileDir = "cardinal_files"
-
-  private val rootGridPane = new GridPane()
-  rootGridPane.getColumnConstraints.add(ColumnConstraintsBuilder().withHgrow(Priority.ALWAYS).withPercentageWidth(25).build)
-  rootGridPane.getColumnConstraints.add(ColumnConstraintsBuilder().withHgrow(Priority.ALWAYS).withPercentageWidth(75).build)
-  rootGridPane.getRowConstraints.add(RowConstraintsBuilder().withVgrow(Priority.ALWAYS).build)
-
-  private val filePane = new FilePane(loadFile, deleteFile, duplicateFile)
-  private val requestInputPane = new RequestInputPane
-  private val responsePane = new ResponsePane()
-  private val requestControlPane = new RequestControlPane(sendRequestAndLoadResponse, showBulkRequestDialogNoArgs, clearAll, save)
-
   private val httpUtil = new HttpUtil
+  private val cardinalView = new CardinalView(loadFile, deleteFile, clearAll, save, duplicateFile, sendRequest)
 
   override def start(primaryStage: Stage): Unit = {
     primaryStage.setTitle("Cardinal")
     primaryStage.setMinHeight(400)
     primaryStage.setMinWidth(800)
 
-    val grid2 = new GridPane
-    grid2.getColumnConstraints.add(ColumnConstraintsBuilder().withHgrow(Priority.ALWAYS).withPercentageWidth(45).build)
-    grid2.getColumnConstraints.add(ColumnConstraintsBuilder().withHgrow(Priority.ALWAYS).withPercentageWidth(55).build)
-    grid2.getRowConstraints.add(RowConstraintsBuilder().withVgrow(Priority.ALWAYS).build)
-    grid2.getRowConstraints.add(RowConstraintsBuilder().withVgrow(Priority.NEVER).build)
-
-    grid2.add(requestInputPane, 0, 0)
-    grid2.add(responsePane, 1, 0)
-
-    GridPane.setColumnSpan(requestControlPane, 2)
-    grid2.add(requestControlPane, 0, 1)
-
-    rootGridPane.add(filePane, 0, 0)
-    rootGridPane.add(grid2, 1, 0)
-
-    val scene = new Scene(rootGridPane, 1000, 500)
+    val scene = new Scene(cardinalView, 1000, 500)
     primaryStage.setScene(scene)
     primaryStage.show()
-
-    filePane.setListContentTo(IOUtil.listFileNames(fileDir))
-  }
-
-
-  private def showErrorDialog(errorMessage: String): Unit = {
-    val alert = new Alert(AlertType.ERROR)
-    alert.setContentText(errorMessage)
-    alert.showAndWait
-  }
-
-  private def showInputDialog(defaultValue: String = ""): Option[String] = {
-    val alert = new TextInputDialog(defaultValue)
-    alert.setContentText("Please enter filename")
-    val result = alert.showAndWait()
-    if (result.isPresent) {
-      Some(result.get)
-    } else {
-      None
-    }
+    cardinalView.setFileList(IOUtil.listFileNames(fileDir))
   }
 
   private def sendRequest(request: Request): HttpResponseWrapper = {
@@ -95,113 +39,44 @@ class Cardinal extends Application {
     HttpResponseWrapper(response, totalTime)
   }
 
-  private def sendRequestAndLoadResponse(): Unit = {
-    val request = requestInputPane.getRequest
-    if (request.uri.trim.length == 0) {
-      showErrorDialog("Please enter a URL.")
-    } else {
-      try {
-        val httpResponse = sendRequest(requestInputPane.getRequest.processConstants())
-        responsePane.loadResponse(httpResponse)
-      } catch {
-        case _: ConnectException => showErrorDialog("Connection refused.")
-        case _: URISyntaxException => showErrorDialog("Invalid URL.")
-        case _: UnknownHostException => showErrorDialog("Unknown Host.")
-        case _: SSLHandshakeException => showErrorDialog("SSL Handshake failed. Remote host closed connection during handshake.")
-        case _: Exception => showErrorDialog("Unknown error occurred.")
-      }
-    }
-  }
-
-  private def showBulkRequestResultDialog(responses: List[HttpResponseWrapper]): Unit = {
-    Platform.runLater(() => {
-      val alert = new BulkRequestOutputDialog(responses)
-      alert.show()
-    })
-  }
-
-  private def showBulkRequestDialogNoArgs(): Unit = {
-    showBulkRequestDialog()
-  }
-
-  @tailrec
-  private def showBulkRequestDialog(bulkRequest: BulkRequest = BulkRequest()): Unit = {
-    val request = requestInputPane.getRequest
-    if (request.uri.trim.length == 0) {
-      showErrorDialog("Please enter a URL.")
-    } else {
-      val dialog = new BulkRequestInputDialog(bulkRequest)
-      val results = dialog.showAndWait()
-      if (results.isPresent) {
-        val throttle = results.get.throttle
-        val count = results.get.count
-        val ids = results.get.ids
-
-        if (count.isEmpty && ids.isEmpty) {
-          showErrorDialog("Enter a for each value or count value.")
-          showBulkRequestDialog(BulkRequest(throttle, count, ids))
-        } else if (count.isDefined && ids.nonEmpty) {
-          showErrorDialog("Enter only a for each value or count value.")
-          showBulkRequestDialog(BulkRequest(throttle, count, ids))
-        } else {
-          new BulkRequestProcessingDialog(count, throttle, ids, request, sendRequest, showBulkRequestResultDialog).show()
-        }
-      }
-    }
-  }
-
-  private def clearAll(): Unit = {
-    currentFile = None
-    requestInputPane.clear()
-    responsePane.clear()
-  }
-
-  private def save(): Unit = {
-    save(requestInputPane.getRequest)
-  }
-
   private def save(request: Request, filename: Option[String] = None): Unit = {
     if (filename.isDefined) {
       IOUtil.writeToFile(fileDir + "/" + filename.get + ".json", request.toJson)
-      filePane.setListContentTo(IOUtil.listFileNames(fileDir))
+      cardinalView.setFileList(IOUtil.listFileNames(fileDir))
     } else if (currentFile.isDefined) {
       IOUtil.writeToFile( currentFile.get.getPath, request.toJson)
     } else {
-      val result = showInputDialog()
+      val result = cardinalView.showInputDialog()
       if (result.isDefined) {
         IOUtil.writeToFile(fileDir + "/" + result.get + ".json", request.toJson)
-        filePane.setListContentTo(IOUtil.listFileNames(fileDir))
+        cardinalView.setFileList(IOUtil.listFileNames(fileDir))
       }
     }
-  }
-
-  private def loadRequest(request: Request): Unit = {
-    requestInputPane.loadRequest(request)
   }
 
   private def loadFile(filename: String): Unit = {
     try {
-      clearAll()
+      cardinalView.clearRequestResponsePanes()
       currentFile = Some(IOUtil.loadFile(fileDir + "/" + filename + ".json"))
       val data = IOUtil.readFileContents(currentFile.get)
-      loadRequest(Request(data))
+      cardinalView.loadRequest(Request(data))
     } catch {
       case _: Exception =>
-        filePane.setListContentTo(IOUtil.listFileNames(fileDir))
-        showErrorDialog("Error loading: " + filename)
+        cardinalView.setFileList(IOUtil.listFileNames(fileDir))
+        cardinalView.showErrorDialog("Error loading: " + filename)
     }
   }
 
   private def loadFileAsRequest(filename: String): Option[Request] = {
     try {
-      clearAll()
+      cardinalView.clearRequestResponsePanes()
       currentFile = Some(IOUtil.loadFile(fileDir + "/" + filename + ".json"))
       val data = IOUtil.readFileContents(currentFile.get)
       Some(Request(data))
     } catch {
       case _: Exception =>
-        filePane.setListContentTo(IOUtil.listFileNames(fileDir))
-        showErrorDialog("Error loading: " + filename)
+        cardinalView.setFileList(IOUtil.listFileNames(fileDir))
+        cardinalView.showErrorDialog("Error loading: " + filename)
         None
     }
   }
@@ -209,19 +84,24 @@ class Cardinal extends Application {
   private def duplicateFile(filename: String): Unit = {
     val request = loadFileAsRequest(filename)
     if (request.isDefined) {
-      val newName = showInputDialog(filename)
+      val newName = cardinalView.showInputDialog(filename)
       if (newName.isDefined) {
         save(request.get, Some(newName.get))
         currentFile = Some(IOUtil.loadFile(newName.get))
-        filePane.setListContentTo(IOUtil.listFileNames(fileDir))
-        filePane.highlight(newName.get)
+        cardinalView.setFileList(IOUtil.listFileNames(fileDir))
+        cardinalView.highlightFile(newName.get)
       }
     }
   }
 
   private def deleteFile(filename: String): Unit = {
     IOUtil.deleteFile(fileDir + "/" + filename + ".json")
-    filePane.setListContentTo(IOUtil.listFileNames(fileDir))
-    clearAll()
+    cardinalView.setFileList(IOUtil.listFileNames(fileDir))
+    cardinalView.clearRequestResponsePanes()
+  }
+
+  private def clearAll(): Unit = {
+    currentFile = None
+    cardinalView.clearRequestResponsePanes()
   }
 }
---
+++ src/main/scala/uk/co/ridentbyte/view/CardinalView.scala
@@ -0,0 +1,147 @@
+package uk.co.ridentbyte.view
+
+import java.net.{ConnectException, URISyntaxException, UnknownHostException}
+
+import javafx.application.Platform
+import javafx.scene.control.Alert.AlertType
+import javafx.scene.control.{Alert, TextInputDialog}
+import javafx.scene.layout.{GridPane, Priority}
+import javax.net.ssl.SSLHandshakeException
+import uk.co.ridentbyte.model.{BulkRequest, HttpResponseWrapper, Request}
+import uk.co.ridentbyte.view.file.FilePane
+import uk.co.ridentbyte.view.request.{BulkRequestInputDialog, BulkRequestProcessingDialog, RequestControlPane, RequestInputPane}
+import uk.co.ridentbyte.view.response.{BulkRequestOutputDialog, ResponsePane}
+import uk.co.ridentbyte.view.util.{ColumnConstraintsBuilder, RowConstraintsBuilder}
+
+import scala.annotation.tailrec
+
+class CardinalView(loadFile: (String) => Unit,
+                   deleteFile: (String) => Unit,
+                   clearAll: () => Unit,
+                   saveCallback: (Request, Option[String]) => Unit,
+                   duplicateFile: (String) => Unit,
+                   sendRequest: (Request) => HttpResponseWrapper) extends GridPane {
+
+  getColumnConstraints.add(ColumnConstraintsBuilder().withHgrow(Priority.ALWAYS).withPercentageWidth(25).build)
+  getColumnConstraints.add(ColumnConstraintsBuilder().withHgrow(Priority.ALWAYS).withPercentageWidth(75).build)
+  getRowConstraints.add(RowConstraintsBuilder().withVgrow(Priority.ALWAYS).build)
+
+  private val filePane = new FilePane(loadFile, deleteFile, duplicateFile)
+  private val requestInputPane = new RequestInputPane
+  private val responsePane = new ResponsePane()
+  private val requestControlPane = new RequestControlPane(sendRequestAndLoadResponse, showBulkRequestDialogNoArgs, clearAll, save)
+
+  val grid2 = new GridPane
+  grid2.getColumnConstraints.add(ColumnConstraintsBuilder().withHgrow(Priority.ALWAYS).withPercentageWidth(45).build)
+  grid2.getColumnConstraints.add(ColumnConstraintsBuilder().withHgrow(Priority.ALWAYS).withPercentageWidth(55).build)
+  grid2.getRowConstraints.add(RowConstraintsBuilder().withVgrow(Priority.ALWAYS).build)
+  grid2.getRowConstraints.add(RowConstraintsBuilder().withVgrow(Priority.NEVER).build)
+
+  grid2.add(requestInputPane, 0, 0)
+  grid2.add(responsePane, 1, 0)
+
+  GridPane.setColumnSpan(requestControlPane, 2)
+  grid2.add(requestControlPane, 0, 1)
+
+  add(filePane, 0, 0)
+  add(grid2, 1, 0)
+
+
+  def setFileList(fileNames: List[String]): Unit = {
+    filePane.setListContentTo(fileNames)
+  }
+
+  def showInputDialog(defaultValue: String = ""): Option[String] = {
+    val alert = new TextInputDialog(defaultValue)
+    alert.setContentText("Please enter filename")
+    val result = alert.showAndWait()
+    if (result.isPresent) {
+      Some(result.get)
+    } else {
+      None
+    }
+  }
+
+  def showErrorDialog(errorMessage: String): Unit = {
+    val alert = new Alert(AlertType.ERROR)
+    alert.setContentText(errorMessage)
+    alert.showAndWait
+  }
+
+
+  private def showBulkRequestResultDialog(responses: List[HttpResponseWrapper]): Unit = {
+    Platform.runLater(() => {
+      val alert = new BulkRequestOutputDialog(responses)
+      alert.show()
+    })
+  }
+
+  @tailrec
+  private def showBulkRequestDialog(bulkRequest: BulkRequest = BulkRequest()): Unit = {
+    val request = requestInputPane.getRequest
+    if (request.uri.trim.length == 0) {
+      showErrorDialog("Please enter a URL.")
+    } else {
+      val dialog = new BulkRequestInputDialog(bulkRequest)
+      val results = dialog.showAndWait()
+      if (results.isPresent) {
+        val throttle = results.get.throttle
+        val count = results.get.count
+        val ids = results.get.ids
+
+        if (count.isEmpty && ids.isEmpty) {
+          showErrorDialog("Enter a for each value or count value.")
+          showBulkRequestDialog(BulkRequest(throttle, count, ids))
+        } else if (count.isDefined && ids.nonEmpty) {
+          showErrorDialog("Enter only a for each value or count value.")
+          showBulkRequestDialog(BulkRequest(throttle, count, ids))
+        } else {
+          new BulkRequestProcessingDialog(count, throttle, ids, request, sendRequest, showBulkRequestResultDialog).show()
+        }
+      }
+    }
+  }
+
+  def clearRequestResponsePanes(): Unit = {
+    requestInputPane.clear()
+    responsePane.clear()
+  }
+
+
+  private def sendRequestAndLoadResponse(): Unit = {
+    val request = requestInputPane.getRequest
+    if (request.uri.trim.length == 0) {
+      showErrorDialog("Please enter a URL.")
+    } else {
+      try {
+        val httpResponse = sendRequest(requestInputPane.getRequest.processConstants())
+        responsePane.loadResponse(httpResponse)
+      } catch {
+        case _: ConnectException => showErrorDialog("Connection refused.")
+        case _: URISyntaxException => showErrorDialog("Invalid URL.")
+        case _: UnknownHostException => showErrorDialog("Unknown Host.")
+        case _: SSLHandshakeException => showErrorDialog("SSL Handshake failed. Remote host closed connection during handshake.")
+        case _: Exception => showErrorDialog("Unknown error occurred.")
+      }
+    }
+  }
+
+  private def showBulkRequestDialogNoArgs(): Unit = {
+    showBulkRequestDialog()
+  }
+
+
+  def loadRequest(request: Request): Unit = {
+    requestInputPane.loadRequest(request)
+  }
+
+  def highlightFile(filename: String): Unit = {
+    filePane.highlight(filename)
+  }
+
+  private def save(): Unit = {
+    saveCallback(requestInputPane.getRequest, None)
+  }
+
+
+}
