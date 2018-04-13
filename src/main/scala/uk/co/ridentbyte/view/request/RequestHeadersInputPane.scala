d5e9b8b6e290c0017c3da4353806fbafc5d99497
Benjamin Dale <ben@ben-dale.co.uk>
2018-04-13 19:44:25 +0100

    list -> textare for headers input

--- src/main/scala/uk/co/ridentbyte/view/request/RequestHeadersInputPane.scala
+++ src/main/scala/uk/co/ridentbyte/view/request/RequestHeadersInputPane.scala
@@ -1,12 +1,7 @@
 package uk.co.ridentbyte.view.request
 
-import javafx.event.ActionEvent
-import javafx.geometry.HPos
-import javafx.scene.control.{Button, Label, ListView}
-import javafx.scene.control.cell.TextFieldListCell
+import javafx.scene.control.{Label, TextArea}
 import javafx.scene.layout.{GridPane, Priority}
-
-import scala.collection.JavaConverters._
 
 class RequestHeadersInputPane(triggerUnsavedChangesMade: () => Unit) extends GridPane {
 
@@ -20,58 +15,26 @@ class RequestHeadersInputPane(triggerUnsavedChangesMade: () => Unit) extends Gri
   GridPane.setHgrow(labelHeaders, Priority.ALWAYS)
   add(labelHeaders, 0, 0)
 
-  private val listHeaders = new ListView[String]()
-  listHeaders.getItems.addListener((_) => triggerUnsavedChangesMade())
-  listHeaders.setEditable(true)
-  listHeaders.getStyleClass.add("cardinal-font")
-  listHeaders.setCellFactory(TextFieldListCell.forListView())
-  GridPane.setColumnSpan(listHeaders, 2)
-  GridPane.setVgrow(listHeaders, Priority.ALWAYS)
-  GridPane.setHgrow(listHeaders, Priority.ALWAYS)
-  add(listHeaders, 0, 1)
-
-  private val buttonRemoveHeader = new Button("-")
-  buttonRemoveHeader.setOnAction(removeHeaderAction)
-  buttonRemoveHeader.setStyle("-fx-padding: 2 6 2 6;")
-  buttonRemoveHeader.getStyleClass.add("cardinal-font")
-  GridPane.setColumnSpan(buttonRemoveHeader, 1)
-  GridPane.setHalignment(buttonRemoveHeader, HPos.RIGHT)
-  GridPane.setVgrow(buttonRemoveHeader, Priority.NEVER)
-  GridPane.setHgrow(buttonRemoveHeader, Priority.ALWAYS)
-  add(buttonRemoveHeader, 0, 2)
-
-  private val buttonAddHeader = new Button("+")
-  buttonAddHeader.setOnAction(addNewHeaderAction)
-  buttonAddHeader.setStyle("-fx-padding: 2 6 2 6;")
-  buttonAddHeader.getStyleClass.add("cardinal-font")
-  GridPane.setColumnSpan(buttonAddHeader, 1)
-  GridPane.setVgrow(buttonAddHeader, Priority.NEVER)
-  GridPane.setHgrow(buttonAddHeader, Priority.NEVER)
-  add(buttonAddHeader, 1, 2)
+  private val textHeaders = new TextArea
+  textHeaders.textProperty().addListener((_, _, _) => triggerUnsavedChangesMade())
+  textHeaders.getStyleClass.add("cardinal-font")
+  GridPane.setVgrow(textHeaders, Priority.ALWAYS)
+  GridPane.setHgrow(textHeaders, Priority.ALWAYS)
+  add(textHeaders, 0, 1)
 
   def getHeaders: List[String] = {
-    listHeaders.getItems.asScala.toList
+    textHeaders.getText.split("\n").toList
   }
 
   def clear(): Unit = {
-    listHeaders.getItems.clear()
+    textHeaders.setText("")
   }
 
   def addHeader(header: String): Unit = {
-    listHeaders.getItems.add(header)
-  }
-
-  private def removeHeaderAction(actionEvent: ActionEvent): Unit = {
-    val selectedIndex = listHeaders.getSelectionModel.getSelectedIndex
-    if (selectedIndex >= 0) {
-      listHeaders.getItems.remove(selectedIndex)
+    if (textHeaders.getText.trim.isEmpty) {
+      textHeaders.setText(header)
+    } else {
+      textHeaders.setText(textHeaders.getText + "\n" + header)
     }
-  }
-
-  private def addNewHeaderAction(actionEvent: ActionEvent): Unit = {
-    listHeaders.getItems.add("")
-    val index = listHeaders.getItems.size - 1
-    listHeaders.requestFocus()
-    listHeaders.getSelectionModel.select(index)
   }
 }
