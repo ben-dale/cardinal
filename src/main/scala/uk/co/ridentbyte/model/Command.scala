ac1e58f7ac0b1bb1aab6e1cb4863013fbdf23b4d
Benjamin Dale <ben@ben-dale.co.uk>
2018-06-12 18:05:33 +0100

    initial work on improving parsing and validation function args

--- src/main/scala/uk/co/ridentbyte/model/Command.scala
+++ src/main/scala/uk/co/ridentbyte/model/Command.scala
@@ -2,6 +2,8 @@ package uk.co.ridentbyte.model
 
 import java.util.Random
 
+import scala.util.matching.Regex
+
 /**
   * Handles commands in the format: #{(.*)}
   *
@@ -21,13 +23,22 @@ case class Command(private val command: String) {
 
   def rawCommand: String = command
 
-  def processWith(variables: Map[String, () => String], vocabulary: Vocabulary, prev: Option[String] = None, r: Random = new Random()): String = {
+  def processWith(constants: Map[String, () => String], vocabulary: Vocabulary, prev: Option[String] = None, r: Random = new Random()): String = {
     val rawContents = prev.getOrElse(contents)
-    val optDirectlyMatchedVar = variables.find(_._1 == rawContents)
+    val optDirectlyMatchedVar = constants.find(_._1 == rawContents)
     if (optDirectlyMatchedVar.isDefined) {
       optDirectlyMatchedVar.get._2()
     } else {
       // Not a variable, process functions
+
+      val functions = List[Regex](
+        "(?:lower\\((.*)\\))".r,
+        "(?:upper\\((.*)\\))".r,
+        "(?:capitalise\\((.*)\\))".r,
+        "(?:random\\((.*)\\))".r,
+        "(?:lorem\\((.*)\\))".r
+      )
+
 
       val lowerFunction = "(?:lower\\((.*)\\))".r
       val upperFunction = "(?:upper\\((.*)\\))".r
@@ -35,30 +46,46 @@ case class Command(private val command: String) {
       val randomFunction = "(?:random\\((.*)\\))".r
       val loremFunction = "(?:lorem\\((.*)\\))".r
 
-      val rawString = "\"[^\"]+\""
+      val rawString = "\"[^\"]*\""
 
       rawContents match {
         case lowerFunction(v) if v.matches(rawString) => v.replace("\"", "").toLowerCase()
-        case lowerFunction(v) if !v.matches(rawString) => processWith(variables, vocabulary, Some(v)).toLowerCase
+        case lowerFunction(v) if !v.matches(rawString) => processWith(constants, vocabulary, Some(v)).toLowerCase
         case upperFunction(v) if v.matches(rawString) => v.replace("\"", "").toUpperCase()
-        case upperFunction(v) if !v.matches(rawString) => processWith(variables, vocabulary,Some(v)).toUpperCase
+        case upperFunction(v) if !v.matches(rawString) => processWith(constants, vocabulary, Some(v)).toUpperCase
         case capitaliseFunction(v) if v.matches(rawString) => v.replace("\"", "").capitalize
-        case capitaliseFunction(v) if !v.matches(rawString) => processWith(variables, vocabulary, Some(v)).capitalize
+        case capitaliseFunction(v) if !v.matches(rawString) => processWith(constants, vocabulary, Some(v)).capitalize
         case loremFunction(n) if isInt(n) => vocabulary.loremIpsum.first(n.toInt).mkString(" ")
         case loremFunction(n) if !isInt(n) =>
-          val processedContents = processWith(variables, vocabulary, Some(n))
+          val processedContents = processWith(constants, vocabulary, Some(n))
           if (isInt(processedContents)) {
             vocabulary.loremIpsum.first(processedContents.toInt).mkString(" ")
           } else {
             processedContents
           }
         case randomFunction(v) =>
-          val splitValues = v.split(",")
-          val processedValues = splitValues.map { v => processWith(variables, vocabulary, Some(v.trim)) }
-          processedValues(r.nextInt(processedValues.length))
+          val splitValues = v.split(",").map(_.trim).toList
+          val argumentTypes = getArgumentTypes(splitValues, constants, functions)
+          val containsInvalidTypes = argumentTypes.count(n => n.isInstanceOf[IntLiteral] || n.isInstanceOf[InvalidLiteral]) > 0
+          if (!containsInvalidTypes) {
+            val processedValues = splitValues.map { v => processWith(constants, vocabulary, Some(v)) }
+            processedValues(r.nextInt(processedValues.length))
+          } else {
+            command
+          }
         case v if v.matches(rawString) => v.replace("\"", "")
         case v => v
       }
+    }
+  }
+
+  def getArgumentTypes(args: List[String], constants: Map[String, () => String], functions: List[Regex]): List[ArgumentType] = {
+    args.map {
+      case a if a.matches("\"[^\"]*\"") => StringLiteral(a)
+      case i if i.matches("[0-9]+") => IntLiteral(i)
+      case c if constants.contains(c) => Constant(c)
+      case f if functions.exists(f2 => f.matches(f2.regex)) => Function(f)
+      case n => InvalidLiteral(n)
     }
   }
 
@@ -72,3 +99,10 @@ case class Command(private val command: String) {
   }
 
 }
+
+class ArgumentType(value: String)
+case class Function(value: String) extends ArgumentType(value)
+case class Constant(value: String) extends ArgumentType(value)
+case class StringLiteral(value: String) extends ArgumentType(value)
+case class IntLiteral(value: String) extends ArgumentType(value)
+case class InvalidLiteral(value: String) extends ArgumentType(value)
