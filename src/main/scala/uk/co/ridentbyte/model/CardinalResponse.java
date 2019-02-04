03fe078d683ac6ac0b713f67adb844c3d6518eeb
Benjamin Dale <ben@ben-dale.co.uk>
2019-02-04 19:41:41 +0000

    add xml auto pretty printing

--- src/main/scala/uk/co/ridentbyte/model/CardinalResponse.java
+++ src/main/scala/uk/co/ridentbyte/model/CardinalResponse.java
@@ -5,7 +5,23 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonSyntaxException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+import java.io.ByteArrayInputStream;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,6 +44,37 @@ public class CardinalResponse {
                 JsonElement el = parser.parse(this.raw.getBody());
                 return gson.toJson(el);
             } catch (JsonSyntaxException jse) {
+                return this.raw.getBody();
+            }
+        } else if (contentType.contains("application/xml")) {
+            String body = this.raw.getBody();
+            if (body.startsWith("\u00EF\u00BB\u00BF")) {
+                body = body.replaceFirst("\u00EF\u00BB\u00BF", "");
+            }
+
+            try {
+                Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new InputSource(new ByteArrayInputStream(body.getBytes(StandardCharsets.UTF_8))));
+                document.normalize();
+                XPath xPath = XPathFactory.newInstance().newXPath();
+                NodeList nodeList = (NodeList) xPath.evaluate("//text()[normalize-space()='']", document, XPathConstants.NODESET);
+                for (int i = 0; i < nodeList.getLength(); ++i) {
+                    Node node = nodeList.item(i);
+                    node.getParentNode().removeChild(node);
+                }
+
+                // Setup pretty print options
+                TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                transformerFactory.setAttribute("indent-number", 2);
+                Transformer transformer = transformerFactory.newTransformer();
+                transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+                transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+                transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+                StringWriter stringWriter = new StringWriter();
+                transformer.transform(new DOMSource(document), new StreamResult(stringWriter));
+                return stringWriter.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
                 return this.raw.getBody();
             }
         } else {
