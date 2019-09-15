cbd81f3aa229149266ec13b3338193f9647dd581
Ben Dale <ben@ben-dale.co.uk>
2019-09-15 16:50:36 +0100

    updates to build stuff

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module cardinal {
     requires org.apache.httpcomponents.httpcore;
     requires org.apache.httpcomponents.httpclient;
     requires javafx.controls;
-    requires javafx.graphics;
     requires org.apache.commons.io;
     requires gson;
     requires java.xml;
