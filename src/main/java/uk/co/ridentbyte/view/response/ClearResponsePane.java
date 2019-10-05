48adc3999372642d3ac7f76c1d8ae230a64e98c8
Ben Dale <ben@ben-dale.co.uk>
2019-10-05 10:16:28 +0100

    remove welcome message

--- src/main/java/uk/co/ridentbyte/view/response/ClearResponsePane.java
+++ src/main/java/uk/co/ridentbyte/view/response/ClearResponsePane.java
@@ -1,15 +1,11 @@
 package uk.co.ridentbyte.view.response;
 
-import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 
 public class ClearResponsePane extends BorderPane {
     public ClearResponsePane() {
-        var prompt = new Label("Welcome!");
-        prompt.getStyleClass().addAll("cardinal-font-console", "grey-text");
         this.getStyleClass().addAll("dashed-border", "round-border");
-        this.setCenter(prompt);
         this.setBorder(
                 new Border(
                         new BorderStroke(
